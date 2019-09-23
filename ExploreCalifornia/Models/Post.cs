811168a19edfbe178bcb6e8a17b2dd17bd89eaf3
Anders Østergaard Laursen <anderslaursen7@gmail.com>
2019-09-23 17:46:40 +0200

    Database Connection

--- ExploreCalifornia/Models/Post.cs
+++ ExploreCalifornia/Models/Post.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.Models
@@ -10,6 +11,26 @@ namespace ExploreCalifornia.Models
     
     public class Post
     {
+        public long id { get; set; }
+
+        private string _key;
+
+        public string key
+        {
+            get
+            {
+                if(_key == null)
+                {
+                    _key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _key;               
+            }
+            set { _key = value; }
+            
+
+            
+        }
+
         [Display(Name = "Post Title")]
         [Required]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters long")]
